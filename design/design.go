4f82b483c89ca58b756d1a781db12e1637cfefd0
Harry Tucker <harrytuckerubuntu@gmail.com>
2019-11-20 20:05:29 +0000

    Initial commit.

---
+++ design/design.go
@@ -0,0 +1,40 @@
+package design
+
+import (
+	. "goa.design/goa/v3/dsl"
+)
+
+var _ = API("Hello world!", func() {
+	Title("Hello world!")
+	Description("Example API that demonstrates REST & gRPC.")
+	Server("Hello world!", func() {
+		Host("localhost", func() {
+			Services("Example Service")
+
+			URI("http://localhost:8000")
+			URI("grpc://localhost:8080")
+		})
+	})
+})
+
+// Service defines a group of remotely accessible methods that are hosted together.
+// The service DSL makes it possible to define the methods, their input and output
+// as well as the errors they may return independently of the underlying transport (HTTP or gRPC).
+var _ = Service("Example Service", func() {
+	Description("The calc service performs operations on numbers.")
+
+	// Method defines a single service method.
+	Method("Say Hello", func() {
+		Result(String)
+
+		HTTP(func() {
+			// route and method declaration
+			GET("/hello/world")
+		})
+
+		GRPC(func() {
+		})
+	})
+
+	Files("/openapi.json", "./gen/http/openapi.json")
+})
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/harrytucker/hello-world-goa
+
+go 1.13
+
+require (
+	github.com/gorilla/websocket v1.4.1 // indirect
+	goa.design/goa/v3 v3.0.7
+	golang.org/x/net v0.0.0-20191119073136-fc4aabc6c914 // indirect
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+	golang.org/x/tools v0.0.0-20191120190209-cefbc64fbd47 // indirect
+	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11 // indirect
+	google.golang.org/grpc v1.25.1 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+)
